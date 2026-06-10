52115b5a4b5790378d9da191e77e341bce808a11
enclave <k.s.guzenko@yandex.ru>
2026-06-10 20:44:45 +0700

    feat: add command ui primitive (ncm-d7z3)

---
+++ ui/src/lib/components/ui/command/command-dialog.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import type { Command as CommandPrimitive } from "bits-ui";
+	import type { Snippet } from "svelte";
+	import Command from "./command.svelte";
+	import * as Dialog from "$lib/components/ui/dialog/index.js";
+	import { cn } from "$lib/utils.js";
+
+	let {
+		open = $bindable(false),
+		ref = $bindable(null),
+		value = $bindable(""),
+		title = "Command Palette",
+		description = "Search for a command to run...",
+		children,
+		class: className,
+		...restProps
+	}: CommandPrimitive.RootProps & {
+			open?: boolean;
+			children: Snippet;
+			title?: string;
+			description?: string;
+			class?: string;
+		} = $props();
+</script>
+
+<Dialog.Root
+	bind:open
+	{title}
+	{description}
+	class={cn("overflow-hidden p-0", className)}
+>
+	<Command {...restProps} bind:value bind:ref {children} />
+</Dialog.Root>
